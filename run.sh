780dbf64fb01adeaeef9fcd5fb509ae8fffb7c23
Rakan Kandah <rakankandah@gmail.com>
2020-12-30 16:27:35 -0800

    updated .sh files

--- run.sh
+++ run.sh
@@ -9,8 +9,15 @@ cd battleship
 
 sudo apt-get update -y && sudo apt-get install -y linux-image-extra-$(uname -r)
 sudo apt-get install docker-engine -y
+sudo groupadd docker
+sudo usermod -aG docker $(whoami)
 sudo service docker start
 sudo docker run hello-world
-docker build -t battleship .
+sudo service docker start
 
+docker build -t battleship .
+clear
+echo "We're ready for you now!"
+sleep 1s
+clear
 docker run -it battleship
--- run_on_mac.sh
+++ run_on_mac.sh
@@ -9,6 +9,15 @@ cd battleship
 
 brew cask install docker
 
+open -a Docker
+echo "Please wait while we get everything ready!"
+sleep 15s
+echo "Almost done!"
+sleep 15s
+echo "..."
+sleep 5s
 docker build -t battleship .
-
+echo "We're ready for you now!"
+sleep 1s
+clear
 docker run -it battleship
